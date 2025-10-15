61f7874916f3a58988ca88e91c1752b41fd59269
Anton Sadovnikov <sadovnikoff@gmail.com>
2025-10-15 11:23:40 +0300

    +refactore WIP: server creating and console logging;

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod handlers;
 mod routes;
 
 use log::{debug, error, log_enabled, info, Level};
+use std::net::SocketAddr;
 
 use crate::models::RenderRequest;
 use crate::models::HealthcheckResponse;
@@ -12,7 +13,30 @@ use crate::routes::routes;
 #[tokio::main]
 async fn main()
 {
+    pretty_env_logger::init();
+
+    let routes = routes();
+
+    let addr: SocketAddr = "0.0.0.0:80".parse().expect("Incorrect address");
+
+    info!("🔧 Initializing server...");
+
+    let server = warp::serve(routes)
+        .bind(addr)
+        .await
+        .graceful(async move {
+            let _ = tokio::signal::ctrl_c()
+                .await;
+            info!("📛 Shutdown signal received");
+        });
+
     println!("🚀 Enso ML API server started successfully");
-    warp::serve(routes()).run(([0, 0, 0, 0], 80)).await;
-    info!("Gracefully stopped")
+    info!("📡 Server listening on http://{}",addr);
+    info!("🌐 Local access: http://localhost:{}", addr.port());
+    info!("Press Ctrl+C to stop");
+
+    server.run()
+        .await;
+
+    info!("✅ Server stopped gracefully");
 }
